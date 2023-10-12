b2acc0e8a446d42bfdcf647a2168703ad45049b2
tahsin005 <tahsin.ferdous3546@gmail.com>
2023-10-13 02:47:44 +0600

    javaforuniversity

--- In class/arithmatic.java
+++ In class/arithmatic.java
@@ -1,7 +1,7 @@
 public class arithmatic {
     public static void main(String[] args) {
         int n = 10;
-        int m = 20;
+        int m = 20; 
         int sum = n + m;
         System.out.println("Sum is "+ sum);
     }
